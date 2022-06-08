42e17ec4846a1f89c99c39dbbc0daf685c13fbd9
Leandro Istarion <leandroi@vo-l1.msk.21-school.ru>
2022-06-08 12:42:32 +0300

    tests

---
+++ grep/test_grep.sh
@@ -0,0 +1,218 @@
+#!/bin/bash
+
+COUNTER_SUCCESS=0
+COUNTER_FAIL=0
+DIFF_RES=""
+echo "" > log.txt
+
+for var in -v -c -l -n -h -s -o -i
+do
+  for var2 in -v -c -l -n -h -s -o -i
+  do
+      for var3 in -v -c -l -n -h -s -o -i
+      do
+        if [ $var != $var2 ] && [ $var2 != $var3 ] && [ $var != $var3 ]
+        then
+          TEST1="for s21_grep.c Makefile $var $var2 $var3"
+          echo "$TEST1"
+          ./s21_grep $TEST1 > s21_grep.txt
+          grep $TEST1 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST1" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST2="for s21_grep.c $var $var2 $var3"
+          echo "$TEST2"
+          ./s21_grep $TEST2 > s21_grep.txt
+          grep $TEST2 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST2" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST3="-e for -e ^int s21_grep.c Makefile $var $var2 $var3"
+          echo "$TEST3"
+          ./s21_grep $TEST3 > s21_grep.txt
+          grep $TEST3 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST3" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST4="-e for -e ^int s21_grep.c $var $var2 $var3"
+          echo "$TEST4"
+          ./s21_grep $TEST4 > s21_grep.txt
+          grep $TEST4 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST4" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST5="-e regex -e ^print s21_grep.c $var $var2 $var3 -f pattern.txt"
+          echo "$TEST5"
+          ./s21_grep $TEST5 > s21_grep.txt
+          grep $TEST5 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST5" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST6="-e while -e void s21_grep.c Makefile $var $var2 $var3 -f pattern.txt"
+          echo "$TEST6"
+          ./s21_grep $TEST6 > s21_grep.txt
+          grep $TEST6 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST6" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST7="-e regex -e ^print s21_grep.c $var $var2 $var3 -f pattern2.txt"
+          echo "$TEST7"
+          ./s21_grep $TEST7 > s21_grep.txt
+          grep $TEST7 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST7" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST8="-e while -e void s21_grep.c Makefile $var $var2 $var3 -f pattern2.txt"
+          echo "$TEST8"
+          ./s21_grep $TEST8 > s21_grep.txt
+          grep $TEST8 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST8" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST9="-e regex -e ^print s21_grep.c $var $var2 $var3 -f pattern3.txt"
+          echo "$TEST9"
+          ./s21_grep $TEST9 > s21_grep.txt
+          grep $TEST9 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST9" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST10="-e while -e void s21_grep.c Makefile $var $var2 $var3 -f pattern3.txt"
+          echo "$TEST10"
+          ./s21_grep $TEST10 > s21_grep.txt
+          grep $TEST10 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST10" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST11="-e regex -e ^print s21_grep.c $var $var2 $var3 -f pattern.txt -f pattern2.txt"
+          echo "$TEST11"
+          ./s21_grep $TEST11 > s21_grep.txt
+          grep $TEST11 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST11" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST12="-e while -e void s21_grep.c Makefile $var $var2 $var3 -f pattern.txt -f pattern2.txt"
+          echo "$TEST12"
+          ./s21_grep $TEST12 > s21_grep.txt
+          grep $TEST12 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST12" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST13="-e regex -e ^print s21_grep.c $var $var2 $var3 -f pattern.txt -f pattern2.txt pattern3.txt"
+          echo "$TEST13"
+          ./s21_grep $TEST13 > s21_grep.txt
+          grep $TEST13 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST13" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+          TEST14="-e while -e void s21_grep.c Makefile $var $var2 $var3 -f pattern.txt -f pattern2.txt pattern3.txt"
+          echo "$TEST14"
+          ./s21_grep $TEST14 > s21_grep.txt
+          grep $TEST14 > grep.txt
+          DIFF_RES="$(diff -s s21_grep.txt grep.txt)"
+          if [ "$DIFF_RES" == "Files s21_grep.txt and grep.txt are identical" ]
+            then
+              (( COUNTER_SUCCESS++ ))
+            else
+              echo "$TEST14" >> log.txt
+              (( COUNTER_FAIL++ ))
+          fi
+          rm s21_grep.txt grep.txt
+
+        fi
+      done
+  done
+done
+
+echo "SUCCESS: $COUNTER_SUCCESS"
+echo "FAIL: $COUNTER_FAIL"
